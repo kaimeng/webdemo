ff7e2d3350ac0822a2c14a7b476f3ef82c14a964
kaimeng <kai_meng@sina.cn>
2014-12-01 23:41:10 +0800

    test

---
+++ webdemo/src/main/java/com/test/webdemo/bmo/TestService.java
@@ -0,0 +1,7 @@
+package com.test.webdemo.bmo;
+
+/**
+ * Created by Meng on 2014/12/1.
+ */
+public interface TestService {
+}
---
+++ webdemo/src/main/java/com/test/webdemo/controller/TestController.java
@@ -0,0 +1,7 @@
+package com.test.webdemo.controller;
+
+/**
+ * Created by Meng on 2014/12/1.
+ */
+public class TestController {
+}
